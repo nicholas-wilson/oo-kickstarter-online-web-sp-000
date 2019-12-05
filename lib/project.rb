67a84c0c9cc5373a87852cccab957253457316fc
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-05 02:43:54 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,14 +8,5 @@ class Project
 
   def add_backer(backer)
     @backers << backer
-    has_project = false
-    backer.projects.each do |project|
-      if project.backers.include?(backer)
-        has_project = true
-      end
-    end
-    if !has_project
-      backer.add_project(self)
-    end
   end
 end
