e7c30e02a59444ed0a86d594e82addfbb26a728d
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-05 02:36:40 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,21 @@
 class Project
-  attr_reader :title, :backers
+  attr_accessor :title, :backers
 
   def initialize(title)
     @title = title
     @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    has_project = false
+    backer.projects.each do |project|
+      if project.backers.include?(backer)
+        has_project = true
+      end
+    end
+    if !has_project
+      backer.projects << self
+    end
   end
 end
